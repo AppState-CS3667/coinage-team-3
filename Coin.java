ce292620c09f95a24d0f78aff11ef257bcf2e10d
pedersenjs <pedersenjs@appstate.edu>
2021-11-03 10:33:56 -0400

    Coin.java adding the contructor and global variables

--- Coin.java
+++ Coin.java
@@ -1,3 +1,30 @@
+/**
+ * Coin.java
+ * Coin abstraction
+ * @author Jenna Pedersen
+ * @Version Version 1, Fall 2021
+ */
 public abstract class Coin {
+    protected double value;
+    protected String country;
+    protected String motto;
+    protected String metallicComposition;
+    protected int mintYear;
 
+
+/**
+ * Constructor
+ * @param value is the value of the coin
+ * @param country is the country of origin
+ * @param motto is the motto on th coin
+ * @param metallicComposition is what metals the coin is made out of
+ * @param mintYear is the year that the coin was minted
+ */
+    public Coin(double value, String country, String motto, String metallicComposition, int mintYear){
+        value = this.value;
+        country = this.country;
+        motto = this.motto;
+        metallicComposition = this.metallicComposition;
+        mintYear = this.mintYear;
+    }
 }
