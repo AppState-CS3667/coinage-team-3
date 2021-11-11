39f384d4b7f3f5720f0b9d1838e995c721513d88
Sophie Columbia <sophiecolumbia@gmail.com>
2021-11-10 21:24:34 -0500

    Finished Demo checkstyle.

--- Demo.java
+++ Demo.java
@@ -7,22 +7,22 @@ import java.util.Scanner;
  * @author Will O'Brien
  * @version 11/10/21
  */
-public class Demo{
+public class Demo {
     /**
     * Main method for demo.
     * 
     * @param args command line args
     */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("This is the coin demo class.");
-        System.out.println("What country's coins would you like "+
-                           "to create? Type \"USD\" or \"CAD\".");
+        System.out.println("What country's coins would you like "
+                           + "to create? Type \"USD\" or \"CAD\".");
         Scanner s = new Scanner(System.in);
         String country = s.nextLine();
-        if (country.equals("USD")){
+        if (country.equals("USD")) {
             USDMintFactory.main(new String[1]);
         }
-        else if (country.equals("CAD")){
+        else if (country.equals("CAD")) {
             CADMintFactory.main(new String[1]);
         }
         else {
