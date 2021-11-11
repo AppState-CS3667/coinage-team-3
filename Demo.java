6c9c1511fcf44bde00d5fc3e56e26ca88c0c961b
Will O'Brien <will.robrien01@gmail.com>
2021-11-10 20:15:16 -0500

    Fixed compilation error and began work on Demo.

--- Demo.java
+++ Demo.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Demo{
     public static void main(String[] args){
         System.out.println("This is the coin demo class.");
         System.out.println("What country's coins would you like "+
-                           "to create?")
+                           "to create? Type \"USD\" or \"CAD\".");
+        Scanner s = new Scanner(System.in);
+        String country = s.nextLine();
+        MintFactory mf;
+        if (country.equals("USD")){
+            mf = USDMintFactory.getInstance();
+        }
+        else if (country.equals("CAD")){
+            mf = CADMintFactory.getInstance();
+        }
+        else {
+            System.out.println("Sorry, that input doesn't match.");
+        }
+        
     }
 }
