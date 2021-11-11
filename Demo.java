3471fa1a00085bb78d32eb3e65f39aecc145440a
Will O'Brien <will.robrien01@gmail.com>
2021-11-10 20:05:02 -0500

    Created the demo branch.

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 public class Demo{
     public static void main(String[] args){
         System.out.println("This is the coin demo class.");
+        System.out.println("What country's coins would you like "+
+                           "to create?")
     }
 }
